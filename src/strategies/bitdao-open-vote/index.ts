68e5c369720670c5e9be80072de61c3a00953ce2
CJ <CjHare@users.noreply.github.com>
2021-10-13 21:57:14 +1000

    open voting

---
+++ src/strategies/bitdao-open-vote/index.ts
@@ -0,0 +1,69 @@
+import { BigNumberish } from '@ethersproject/bignumber';
+import { formatUnits } from '@ethersproject/units';
+import { keccak256 } from '@ethersproject/keccak256';
+import { toUtf8Bytes } from '@ethersproject/strings';
+import { Multicaller } from "../../utils";
+
+export const author = 'cjhare';
+export const version = '0.1.0';
+
+const abi = [
+  'function getVotes(address account) public view returns (uint256)'
+];
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+) {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  await validatePresence(options, 'An options object is expected');
+  await validatePresence(options.address, 'Address is required in the options');
+  await validatePresence(
+    options.decimals,
+    'Decimal is required in the options'
+  );
+
+  //TODO remove - if/when new block works
+
+  const multi = new Multicaller(network, provider, abi, { blockTag });
+  addresses.forEach((address) =>
+    multi.call(address, options.address, 'getVotes', [address])
+  );
+
+  const result: Record<string, BigNumberish> = await multi.execute();
+
+  return Object.fromEntries(
+    Object.entries(result).map(([address, balance]) => [
+      address,
+      parseFloat(formatUnits(balance, options.decimals))
+    ])
+  );
+
+/*
+//TODO the abi isn't making it through - the 'getVotes' is lost
+
+  const response: BigNumber[] = await multicall(
+    network,
+    provider,
+    abi,
+    addresses.map((address: any) => [[address.toLowerCase(), role]]),
+    { blockTag }
+  );
+  return Object.fromEntries(
+    response.map((value, i) => [
+      addresses[i],
+      parseFloat(formatUnits(value.toString(), options.decimals))
+    ])
+  );*/
+}
+
+async function validatePresence(parameter: any, reason: string): Promise<void> {
+  if (parameter === undefined) {
+    return Promise.reject(reason);
+  }
+}
--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -5,7 +5,8 @@ import * as balancer from './balancer';
 import * as balancerErc20InternalBalanceOf from './balancer-erc20-internal-balance-of';
 import * as sunder from './sunder';
 import * as balancerSmartPool from './balancer-smart-pool';
-import * as bitdaoRoleVoting from './bitdao-role-voting';
+import * as bitdaoOpenVote from './bitdao-open-vote';
+import * as bitdaoRoleVote from './bitdao-role-vote';
 import * as contractCall from './contract-call';
 import * as dextfVaults from './dextf-staked-in-vaults';
 import * as dfynFarms from './dfyn-staked-in-farms';
@@ -179,7 +180,8 @@ const strategies = {
   'balancer-smart-pool': balancerSmartPool,
   'balancer-erc20-internal-balance-of': balancerErc20InternalBalanceOf,
   'balance-in-vdfyn-vault': vDfynVault,
-  'bitdao-role-voting': bitdaoRoleVoting,
+  'bitdao-open-vote': bitdaoOpenVote,
+  'bitdao-role-vote': bitdaoRoleVote,
   'erc20-received': erc20Received,
   'contract-call': contractCall,
   'dextf-staked-in-vaults': dextfVaults,
